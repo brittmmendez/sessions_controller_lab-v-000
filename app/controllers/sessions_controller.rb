4715abd343a7984cc706e5aa054dacb2b35a053e
Brittany Morris <brittmmendez@gmail.com>
2018-03-01 01:49:33 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    if session[:name] ==nil
+      render :new
+    else
+      session[:name]=params[:name]
+      redirect_to '/'
+  end
+
+  def destroy
+
+  end
 end
